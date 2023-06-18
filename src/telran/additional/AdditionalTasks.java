08cf3e1e3d29edb0a07965b790912bbdbbd12a7c
oljas <olja.smirnov93@gmail.com>
2023-06-18 19:25:00 +0300

    Add# 3 & 4

--- src/telran/additional/AdditionalTasks.java
+++ src/telran/additional/AdditionalTasks.java
@@ -1,33 +1,50 @@
 package telran.additional;
 
+import java.util.Arrays;
+
 public class AdditionalTasks {
-	public static int maxSubsequenceLength(int[] arr) {
-		int res = 0;
-		int count = 1;
-		if(arr.length == 0) {
-			count = 0;
-		} else {
-			for(int i = 0; i < arr.length - 1; i++) {
-				if(arr[i] == arr[i+1]) {
-					count++;
-					if(count >= res) {
-						res = count; 
+	public static boolean isAnagram(String sourceWord, String targetWord) {
+		if(sourceWord.length() != targetWord.length()) {
+			return false;
+		}
+		int [] helper = new int [26];
+		sourceWord = sourceWord.toLowerCase();
+		targetWord = targetWord.toLowerCase();
+		boolean res = true;
+		for (int i = 0; i < sourceWord.length(); i++) {
+				if(sourceWord.charAt(i) < 'a' ||sourceWord.charAt(i) > 'z' && 
+						targetWord.charAt(i) < 'a' || targetWord.charAt(i) > 'z') 
+				{
+					return false;
+				}
+				helper[sourceWord.charAt(i) - 'a'] ++;
+				helper[targetWord.charAt(i) - 'a'] --;
+		}
+		for(int i = 0; i < helper.length; i++) {
+			if(helper[i] != 0) {
+				return false;
+			}
+		}
+		return  res;
+	}
+
+	public static boolean hasValidBrackets(String text) {
+		int j = -1;
+		boolean res = false;
+		char[] stack = new char[text.length()];
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == '(' || text.charAt(i) == '{' || text.charAt(i) == '[') {
+				stack[++j] = text.charAt(i);
+			} else {
+				if (j >= 0 && ((stack[j] == '(' && text.charAt(i) == ')') || (stack[j] == '{' && text.charAt(i) == '}')
+						|| (stack[j] == '[' && text.charAt(i) == ']'))) {
+					stack[j--] = 0;
+					if (stack[0] == 0) {
+						res = true;
 					}
-				} else {
-					count = 1;
 				}
 			}
 		}
-		return  res > count ? res : count;
-	}
-	public static String toBinaryStr(int number) {
-		int rem = 0;
-		String res = "";
-		do {
-			rem = number % 2;
-			number /= 2;
-			res = rem + res;
-		}while(number != 0);
 		return res;
 	}
 }
--- src/telran/additional/test/AdditionalTests.java
+++ src/telran/additional/test/AdditionalTests.java
@@ -2,33 +2,28 @@ package telran.additional.test;
 
 import org.junit.jupiter.api.Test;
 
-import telran.additional.AdditionalTasks;
+import static org.junit.Assert.*;
+import static telran.additional.AdditionalTasks.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-
-class AdditionalTest {
+public class AdditionalTests {
 
 	@Test
-	void maxSubsequenceLengthTest() {
-		int [] arr = {1, 3, 3, 2, 8, 8, 8, 5, 6, 6};
-		int [] arr1 = new int[0];
-		int [] arr2 = {1, 10, 10, 3, 5, 5, 8, 6, 6, 6, 6};
-		int [] arr3 = {5, 5, 5, 5, 5, 5, 5, 1, 8, 3, 3, 3, 3, 9};
-		assertEquals(3, AdditionalTasks.maxSubsequenceLength(arr));
-		assertEquals(0, AdditionalTasks.maxSubsequenceLength(arr1));
-		assertEquals(4, AdditionalTasks.maxSubsequenceLength(arr2));
-		assertEquals(7, AdditionalTasks.maxSubsequenceLength(arr3));
+	void isAnagramTest() {
+		String str1 = "HEART";
+		String str2 = "earth";
+		String str3 = "TRIANGLE";
+		String str4 = "INTEGRAL";
+		assertTrue(isAnagram(str1, str2));
+		assertTrue(isAnagram(str3, str4));
+		assertFalse(isAnagram(str1, str3));
 	}
 	@Test
-	void toBinaryStrTest() {
-		int num = 256;
-		assertEquals(Integer.toBinaryString(num), AdditionalTasks.toBinaryStr(num));
-		num = 0;
-		assertEquals(Integer.toBinaryString(num), AdditionalTasks.toBinaryStr(num));
-		num = 27640;
-		assertEquals(Integer.toBinaryString(num), AdditionalTasks.toBinaryStr(num));
-		num = 178937;
-		assertEquals(Integer.toBinaryString(num), AdditionalTasks.toBinaryStr(num));
+	void hasValidBracketsTest() {
+		String str1 =  "aaaaaa (sdfsdfdsf[dfd(f)f] zcvzxcv {{[ghjk]}} asd )";
+		String str2 = ")dfgswfgsf(";
+		String str3 = "[werwert(wertwrtw] wertrt)";
+		assertTrue(hasValidBrackets(str1));
+		assertFalse(hasValidBrackets(str2));
+		assertFalse(hasValidBrackets(str3));
 	}
 }
